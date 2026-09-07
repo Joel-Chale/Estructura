2894687cae7fec67018328c15088360b4670f127
Joel Chalé <joelchale31@gmail.com>
2026-09-07 12:38:06 -0600

    no se

--- Vectores.py
+++ Vectores.py
@@ -1,13 +1,23 @@
-class Vectores:
+def mostrar_vector(datos):
+    for i in range(len(datos)):
+        print(datos[i])
 
-    pares = [2,3,6,8,10]
-    impares = [1,3,5,7,9]
+def media(datos):
+    n = len(datos)
+    suma = 0
+    for i in range(n):
+        suma = suma + datos[i]
+    return suma / n
 
-    def mostrarVectores(lista):
-        for elemento in lista:
-            print(elemento)
 
-            
-    mostrarVectores(pares)
+if __name__ == "__main__":
+    pares = [2, 4, 6, 8, 10]
+    impares = [1, 3, 5, 7, 9]
+
+    mostrar_vector(pares)
+    print("Media= " + str(media(pares)))
+    
+    mostrar_vector(impares)
+    print("Media= " + str(media(impares)))
 
     

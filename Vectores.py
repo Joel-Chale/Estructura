80970c47c8fdb8febebf38b7bd94c0ff9feec246
Joel Chalé <joelchale31@gmail.com>
2026-09-07 12:28:28 -0600

    SI

---
+++ Vectores.py
@@ -0,0 +1,13 @@
+class Vectores:
+
+    pares = [2,3,6,8,10]
+    impares = [1,3,5,7,9]
+
+    def mostrarVectores(lista):
+        for elemento in lista:
+            print(elemento)
+
+            
+    mostrarVectores(pares)
+
+    

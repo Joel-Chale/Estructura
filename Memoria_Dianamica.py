86e42a6240998ba941bb74bc58bd6a93a7a440e1
Joel Chalé <joelchale31@gmail.com>
2026-09-08 11:35:54 -0600

    Correción

---
+++ Memoria_Dianamica.py
@@ -0,0 +1,19 @@
+def memoria_dinamica():
+    frutas = []
+
+    frutas.append("Mango")
+    frutas.append("Manzana")
+    frutas.append("Banana")
+    frutas.append("Uvas")
+
+    print(frutas)
+
+    frutas.pop(0)  
+    frutas.pop(1)  
+
+    frutas.append("sandia")
+
+    print(frutas)
+
+if __name__ == "__main__":
+    memoria_dinamica()
